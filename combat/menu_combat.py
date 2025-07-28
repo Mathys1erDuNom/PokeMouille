1b727685927d1406fede33e980a1fda9eb71c27e
MathStrike <92716055+MathStrike@users.noreply.github.com>
2025-07-28 18:43:32 -0300

    Ajout du système de combat tour par tour dans le dossier combat/

--- combat/menu_combat.py
+++ combat/menu_combat.py
@@ -68,7 +68,7 @@ class ValidateButton(Button):
         all_captures = get_captures(user_id)
         selected_pokemons = [p for p in all_captures if p["name"] in all_selected]
 
-        bot_team = [poke for poke in full_pokemon_data if poke["name"] in ["Salameche", "Roucool", "Rattata"]]
+        bot_team = [poke for poke in self.parent_view.full_pokemon_data if poke["name"] in ["Salameche", "Roucool", "Rattata"]]
 
 
 
