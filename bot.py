e0c8c7daa47aee4c1db0857837681860ddb258d5
MathStrike <92716055+MathStrike@users.noreply.github.com>
2025-07-28 14:33:52 -0300

    moi con

--- bot.py
+++ bot.py
@@ -6,9 +6,6 @@ import random, asyncio, os, json
 from dotenv import load_dotenv
 import time
 from PIL import Image, ImageDraw, ImageFont
-from combats.combat_logic import setup_battle_commands
-
-
 
 
 
@@ -60,7 +57,6 @@ DEFAULT_SHINY_RATE = 64
 
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-setup_battle_commands(bot)
 
 
 
