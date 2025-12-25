654b158346fd019a1e4e1ec62916bd1e4bc695cb
MathStrike <92716055+MathStrike@users.noreply.github.com>
2025-12-25 13:54:46 +0100

    position poke pokedex

--- new_pokedex.py
+++ new_pokedex.py
@@ -191,7 +191,7 @@ class PokemonButton(Button):
             font = ImageFont.load_default()
             font_bold = font
 
-        pos_nom_type = (90, 100)
+        pos_nom_type = (90, 120)
         pos_ivs = (90, 260)
         pos_stats = (90, 420)
         pos_sprite = (550, 165)
