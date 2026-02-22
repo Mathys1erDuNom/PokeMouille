0c6e61d55fc5ac7b383e34eab947145fdb5805a3
MathStrike <92716055+MathStrike@users.noreply.github.com>
2026-02-22 11:37:10 +0100

    bug ordre combat

--- bot.py
+++ bot.py
@@ -744,7 +744,7 @@ setup_guess_pokemon_command(
 
 bot.is_under_ban = is_under_ban
 setup_pokedex(bot, full_pokemon_shiny_data, full_pokedex, type_sprites, attack_type_map, json_dir)
-setup_new_pokedex(bot, full_pokemon_shiny_data, full_pokedex, type_sprites, attack_type_map, json_dir)
+setup_new_pokedex(bot, full_pokemon_shiny_data, full_pokemon_data, type_sprites, attack_type_map, json_dir)
 
 print("[DEBUG] Ready to run bot...")
 
