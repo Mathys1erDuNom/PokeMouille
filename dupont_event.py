84238211abfd35512ddcf554f3719037b90fd9ea
MathStrike <92716055+MathStrike@users.noreply.github.com>
2026-05-22 15:12:25 +0200

    k

--- dupont_event.py
+++ dupont_event.py
@@ -68,7 +68,7 @@ tableau_riche = [
         "id": 2,
         "name": "Sophie Dupont",
         "premier_texte": [
-            "Je n'en reviens toujours pas de ce qu'il fait, il est tellement corrompu... J'aimerais avoir l'influence nécessaire pour faire cesser tout cela, mais seul je n'irais pas bien loin.... Tiens, prends cet argent, il est sale et je n'en veux pas.",
+            "Je n'en reviens toujours pas de ce qu'il fait, il est tellement corrompu... J'aimerais avoir l'influence nécessaire pour faire cesser tout cela, mais seul je n'irais pas bien loin... Peut-être avec la police ? (!police) Tiens, prends cet argent, il est sale et je n'en veux pas.",
             "Oh tiens, bonjour. As-tu besoin d'argent ? Tiens, tu peux prendre un peu du mien. De toutes façons, je n'en veux pas, cela ne m'intéresse pas, je ne suis pas comme eux. Je veux simplement faire éclater la vérité et sauver ces pauvres créatures du sort qui les attend...",
             "Je suis sûr que c'est sa faute si Joseph a disparu, je ne peux pas encore le prouver mais j'en suis sûr. Ah tiens, je croyais être seul, je réfléchissais à voix haute, tant que tu es là, tu veux de l'argent ? Je n'en veux pas.",
             "Hey, j'ai une question, comment tu ferais pour dénoncer des choses dont personne n'est au courant et qui sont problématiques ? Mmmh, euh je me suis emporté, tiens prends cet argent, désolé de t'avoir dérangé"
@@ -86,10 +86,10 @@ tableau_riche = [
         "id": 3,
         "name": "Serge Dupont",
         "premier_texte": [
-            "Bonjour ! Mais qui es-tu ? As-tu besoin d'un coup de main ? Ma famille est riche grâce à mon père, qui mène un noble commerce avec le casino ! Mes deux frères aînés travaillent avec lui, mais mon autre frère a toujours été distant, et je n'ai jamais su pourquoi. Sûrement une petite querelle de famille ! Tu me parais sympathique, tiens, cadeau !",
+            "Bonjour ! Mais qui es-tu ? As-tu besoin d'un coup de main ? Ma famille est riche grâce à mon père, qui mène un noble commerce avec le casino ! Mon frère aîné travaille avec lui, mais mon autre soeur a toujours été distant, et je n'ai jamais su pourquoi. Sûrement une petite querelle de famille ! Tu me parais sympathique, tiens, cadeau !",
             "Oh bonjour ! J'aime bien ta tête, tu as l'air d'une personne honnête et respectable. Des gens disent que mon père est un criminel, sans coeur. Je ne comprends pas pourquoi ces bruits courent. Père dit que ce sont des journalistes, et qu'il ne faut pas les écouter ! Tiens, prends cet argent, je n'en manque pas",
-            "Bonjour, j'ai une question. C'est normal les engueulades dans une famille ?  Désolée, je suis un peu préoccupée, car mon frère et père se disputent encore à propos de ces animaux. Je ne sais pas trop de quoi ils parlent, à vrai dire. Ils vont sûrement se réconcilier bientôt ! Merci de m'avoir écouté, tiens !",
-            "Bonjour, tu n'aurais pas vu un Ramoloss ? Je ne trouve plus mon Ramoloss et ça me fait peur. Père dit qu'il s'est enfui lors d'une balade... mais je veux le retrouver ! On m'a dit qu'il y avait un parc dans la région de Kanto où on pouvait retrouver des Pokémon perdus... Si tu y vas un jour, tu pourrais peut-être demander aux gardes du parc (!parc)s'ils n'ont rien trouvé ? Tu en as vu un ? Non... Tant pis merci quand même, tiens prends cet argent !"
+            "Bonjour, j'ai une question. C'est normal les engueulades dans une famille ?  Désolée, je suis un peu préoccupée, car ma soeur et père se disputent encore à propos de ces animaux. Je ne sais pas trop de quoi ils parlent, à vrai dire. Ils vont sûrement se réconcilier bientôt ! Merci de m'avoir écouté, tiens !",
+            "Bonjour, tu n'aurais pas vu un Ramoloss ? Je ne trouve plus mon Ramoloss et ça me fait peur. Père dit qu'il s'est enfui lors d'une balade... mais je veux le retrouver ! On m'a dit qu'il y avait un parc dans la région de Kanto où on pouvait retrouver des Pokémon perdus... Si tu y vas un jour, tu pourrais peut-être demander aux gardes du parc (!parc) s'ils n'ont rien trouvé ? Tu en as vu un ? Non... Tant pis merci quand même, tiens prends cet argent !"
         ],
         "somme_prendre": 50,
         "texte_fin": [
